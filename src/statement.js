8af7ccdfa90ee3fedba5b48788b21dc85e0815bc
lichuwen <545759585@qq.com>
2020-08-27 14:51:44 +0800

    [karen] refactor:extract UsdFormat function

--- src/statement.js
+++ src/statement.js
@@ -20,15 +20,20 @@ function getAmount(play, perf) {
   return thisAmount;
 }
 
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+function UsdFormat() {
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+  return format;
+}
+
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = UsdFormat();
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = getAmount(play, perf);
