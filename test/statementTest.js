3f91a655627c6fd3e58857f1c272700c917b6780
lichuwen <545759585@qq.com>
2020-08-27 16:53:01 +0800

    [karen] test:add getHtmlStatement test

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,5 +1,6 @@
 const test = require('ava');
 const {statement} = require('../src/statement');
+const {htmlStatement} = require('../src/statement');
 
 const invoice = {
   'customer': 'BigCo',
@@ -198,12 +199,38 @@ test('statement case 7. Customer BigCo has one unknown performance. ', t => {
   }
 });
 
-// t.is(result, '<h1>Statement for BigCo</h1>\n' +
-//     '<table>\n' +
-//     '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
-//     ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
-//     ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
-//     ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
-//     '</table>\n' +
-//     '<p>Amount owed is <em>$1,730.00</em></p>\n' +
-//     '<p>You earned <em>47</em> credits</p>\n');
+test('statement case 7. Customer BigCo has one unknown performance. ', t => {
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 30,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 20,
+      },
+      {
+        'playID': 'othello',
+        'audience': 31,
+      }
+    ],
+  };
+
+  const result = htmlStatement(invoice, plays);
+  const expect = '<h1>Statement for BigCo</h1>\n' +
+      '<table>\n' +
+      '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+      ' <tr><td>Hamlet</td><td>30</td><td>$400.00</td></tr>\n' +
+      ' <tr><td>As You Like It</td><td>20</td><td>$360.00</td></tr>\n' +
+      ' <tr><td>Othello</td><td>31</td><td>$410.00</td></tr>\n' +
+      '</table>\n' +
+      '<p>Amount owed is <em>$1,170.00</em></p>\n' +
+      '<p>You earned <em>5</em> credits</p>\n';
+
+  t.is(result, expect);
+
+});
+
+
